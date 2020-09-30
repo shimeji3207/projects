2b17f66888d6ec46694626737161b12fd06cdbb3
shimeji3207 <72081640+shimeji3207@users.noreply.github.com>
2020-09-30 21:02:11 +0900

    Updated the Hero and Enemy classes
    - Added methods that allows hero objects to level up.
    - Added methods that defines the stats of enemies depending on their type.

--- textrpg/character.py
+++ textrpg/character.py
@@ -5,12 +5,9 @@ class Character:
     def attack(self, opponent):
         display_message("%sの攻撃!" % (self.stats["name"]))
 
-        if opponent.stats["defence"]/2 > self.stats["attack"]:
-            damage = 1
-        else:
-            damage = int((self.stats["attack"] - (opponent.stats["defence"]/2))/opponent.stats["max_hp"])
+        damage = int(self.stats["attack"] - (opponent.stats["defence"]/2))
 
-        if damage == 0:
+        if damage <= 0:
             damage = 1
 
         opponent.stats["hp"] -= int(damage)
@@ -18,9 +15,6 @@ class Character:
         display_message("%sに%sのダメージを与えた。" % (opponent.stats["name"], damage))
 
 class Hero(Character):
-    YES = 1
-    NO = 0
-
     def __init__(self):
         self.stats = {
         "name": "",
@@ -31,9 +25,9 @@ class Hero(Character):
         "hp": 20,
         "attack": 5,
         "defence": 5,
-        "speed": 3
+        "speed": 5
         }
-        self.ran = self.NO
+        self.ran = False
 
     def display_stats(self):
         display_message("""Name: %s
@@ -48,13 +42,44 @@ SPD: %s""" % (self.stats["name"], self.stats["level"], self.stats["exp"], self.s
 
     def run(self, enemy):
 
-        run_probability = int((2 - (self.stats["speed"]/enemy.stats["speed"])) * 100)
+        run_probability = int(((self.stats["speed"]/enemy.stats["speed"]) - 0.5) * 100)
 
         if (run_probability < 10):
             run_probability = 10
 
         if (randint(0, 99) < run_probability):
-            self.ran = self.YES
+            self.ran = True
+        else:
+            display_message("逃げられなかった。")
+
+    def gain_exp(self, gained_exp):
+        display_message("%sの経験値を獲得しました。" % (gained_exp))
+        self.stats["exp"] += gained_exp
+        if (self.stats["exp"] >= self.stats["next_exp"]):
+            self.level_up()
+
+    def level_up(self):
+        levels_increased = 0
+
+        while (self.stats["exp"] >= self.stats["next_exp"]):
+            self.stats["exp"] -= self.stats["next_exp"]
+            self.stats["next_exp"] = int(self.stats["next_exp"] * 1.2)
+            self.stats["level"] += 1
+            levels_increased += 1
+        display_message("レベル%sになりました！" % (self.stats["level"]))
+
+        self.increase_stats(levels_increased)
+        self.display_stats()
+
+    def increase_stats(self, levels_increased):
+        hp_increase = int(self.stats["max_hp"] * 0.2) * levels_increased
+
+        self.stats["max_hp"] += hp_increase
+        self.stats["hp"] += hp_increase
+        self.stats["attack"] += 2 * levels_increased
+        self.stats["defence"] += 1 * levels_increased
+        self.stats["speed"] += 1 * levels_increased
+
 
 class Enemy(Character):
     LEVEL_RANGE = 3
@@ -63,20 +88,23 @@ class Enemy(Character):
         self.stats = {
         "name": "",
         "level": 1,
-        "max_hp": 10,
-        "hp": 10,
-        "attack": 3,
-        "defence": 3,
-        "speed": 2
+        "max_hp": 0,
+        "hp": 0,
+        "attack": 0,
+        "defence": 0,
+        "speed": 0
+        }
+        self.obtainable_exp = 0
+
+    def define_enemy(self, name, player_level):
+        enemy_stats = {
+        "goblin": self.set_goblin_stats,
+        "slime": self.set_slime_stats,
+        "bandit": self.set_bandit_stats
         }
 
-    def define_enemy(self, name):
-        self.stats["name"] = name
-        self.stats["max_hp"] = 10
-        self.stats["hp"] = 10
-        self.stats["attack"] = 3
-        self.stats["defence"] = 3
-        self.stats["speed"] = 2
+        self.set_level(player_level)
+        enemy_stats[name]()
 
     def set_level(self, player_level):
         if (player_level - self.LEVEL_RANGE < 1):
@@ -84,8 +112,35 @@ class Enemy(Character):
         else:
             self.stats["level"] = randint( player_level - self.LEVEL_RANGE, player_level + self.LEVEL_RANGE)
 
+    def set_slime_stats(self):
+        self.stats["name"] = "スライム"
+        self.stats["max_hp"] = 5 + (self.stats["level"] - 1)
+        self.stats["hp"] = self.stats["max_hp"]
+        self.stats["attack"] = 1 + (self.stats["level"] - 1)
+        self.stats["defence"] = 1 + (self.stats["level"] - 1)
+        self.stats["speed"] = 1 + (self.stats["level"] - 1)
+
+        self.obtainable_exp = 3 + self.stats["level"]
+
     def set_goblin_stats(self):
-        self.stats["level"]
+        self.stats["name"] = "ゴブリン"
+        self.stats["max_hp"] = 5 + (self.stats["level"] + 1)
+        self.stats["hp"] = self.stats["max_hp"]
+        self.stats["attack"] = 2 + (self.stats["level"])
+        self.stats["defence"] = 1 + (self.stats["level"])
+        self.stats["speed"] = 2 + (self.stats["level"])
+
+        self.obtainable_exp = 3 + (2 * self.stats["level"])
+
+    def set_bandit_stats(self):
+        self.stats["name"] = "盗賊"
+        self.stats["max_hp"] = 8 + (self.stats["level"] + 1)
+        self.stats["hp"] = self.stats["max_hp"]
+        self.stats["attack"] = 2 + (self.stats["level"])
+        self.stats["defence"] = 2 + (self.stats["level"])
+        self.stats["speed"] = 1 + (self.stats["level"])
+
+        self.obtainable_exp = 5 + (2 * self.stats["level"])
 
 """
 import random, copy
