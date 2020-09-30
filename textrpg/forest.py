5269e54f8e6af1d32faa26fb4b198eee4165fc48
shimeji3207 <72081640+shimeji3207@users.noreply.github.com>
2020-09-30 21:05:40 +0900

    Updated the Forest class
    - Added a method that allows the player after each day.

--- textrpg/forest.py
+++ textrpg/forest.py
@@ -9,23 +9,24 @@ class Forest:
     def __init__(self, player):
         self.times_visited = 0
         self.days_passed = 0
-        self.encounter_rate = 10
+        self.encounter_rate = 6
         self.enemy = Enemy()
         self.player = player
         self.battle = Battle(player, self.enemy)
 
     def travel(self):
+        display_message("～森～")
+
         while(self.days_passed < 3 + self.times_visited):
-            display_message("～%s日目～" % (self.days_passed + 1))
+            display_message("<%s日目>" % (self.days_passed + 1))
 
             self.event()
 
             if self.player.stats["hp"] <= 0:
                 return
 
+            self.rest_heal()
             self.days_passed += 1
-            # Heal by resting for the night
-            self.player.stats["hp"] += 2
 
         display_message("街についた。")
         self.times_visited += 1
@@ -42,4 +43,12 @@ class Forest:
         self.battle.start_battle()
 
     def choose_enemy(self):
-        self.enemy.define_enemy(self.ENEMIES[randint(0,len(self.ENEMIES) - 1)])
+        self.enemy.define_enemy(self.ENEMIES[randint(0,len(self.ENEMIES) - 1)], self.player.stats["level"])
+
+    def rest_heal(self):
+        heal =  int(self.player.stats["max_hp"]*0.1)
+        if (self.player.stats["hp"] + heal > self.player.stats["max_hp"]):
+            heal = self.player.stats["max_hp"] - self.player.stats["hp"]
+            self.player.stats["hp"] += heal
+
+        display_message("夜休んで体力が%s回復しました。" % (heal))
