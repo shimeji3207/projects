3b0044fe579c158968e1c6241180db036bd19f36
shimeji3207 <72081640+shimeji3207@users.noreply.github.com>
2020-10-18 16:30:33 +0900

    Updated to use the new ITEM_INFO from item_info.py

--- textrpg/item.py
+++ textrpg/item.py
@@ -1,95 +1,109 @@
 from system import display_message
+from item_info import ITEM_INFO
 
 class Item:
-    ITEM_INFO = {
-    "small_potion": {
-        "name": "小回復薬",
-        "price": 20,
-        "effect": "体力を20回復",
-        "type": "potion",
-        "stat_change": {
-            "hp": 20
-            }
-        },
-    "leather_chestplate": {
-        "name": "革のチェストプレート",
-        "price": 20,
-        "effect": "DEF+1",
-        "type": "equipment",
-        "equip_region": "chest",
-        "stat_change": {
-            "defence": 1
-            }
-        },
-    "sword": {
-        "name": "普通の剣",
-        "price": 20,
-        "effect": "ATK+1",
-        "type": "equipment",
-        "equip_region": "right_hand",
-        "stat_change": {
-            "attack": 1
-            }
-        }
-    }
-
     def __init__(self, player = None, enemy = None):
         self.player = player
         self.enemy = enemy
         self.item_name = None
 
+    def find_longest_equipment_name(self):
+        longest_name_length = len("無し")
+        for equipment in self.player.equipped_items.values():
+            if equipment != "none" and len(ITEM_INFO[equipment]["name"]) > longest_name_length:
+                longest_name_length = len(ITEM_INFO[equipment]["name"])
+        return longest_name_length
+
+    def return_item_name(self, item_name):
+        return ITEM_INFO[item_name]["name"]
+
     def return_item_price(self, item_name):
-        return self.ITEM_INFO[item_name]["price"]
-        
+        return ITEM_INFO[item_name]["price"]
+
     def initial_equip(self, item_name):
         self.item_name = item_name
         self.equip_item()
+        
+    def equip_status(self):
+        #display_message("check status")
+        #print(self.player.equipped_items.values())
+        if self.item_name in self.player.equipped_items.values():
+            return True
+        return False
 
     def use_item(self, item_name):
         self.item_name = item_name
 
-        if (self.ITEM_INFO[self.item_name]["type"] == "potion"):
+        if (ITEM_INFO[self.item_name]["type"] == "potion"):
             self.use_potion()
-        elif (self.ITEM_INFO[self.item_name]["type"] == "equipment"):
+        elif (ITEM_INFO[self.item_name]["type"] == "equipment"):
+            if (self.equip_status()):
+                self.unequip_item()
+                return
             self.equip_item()
-            display_message("%sを装備した。" % (self.ITEM_INFO[self.item_name]["name"]))
+            display_message("%sを装備した。" % (ITEM_INFO[self.item_name]["name"]))
 
     def display_item_info(self, item):
-        print("アイテム: %s" % (self.ITEM_INFO[item]["name"]))
-        print("効果: %s" % (self.ITEM_INFO[item]["effect"]))
+        print("アイテム: %s" % (ITEM_INFO[item]["name"]))
+        print("効果: %s" % (ITEM_INFO[item]["effect"]))
 
     def display_item_price(self, item):
-        print("値段: %sゴールド" % (self.ITEM_INFO[item]["price"]))
+        print("値段: %sゴールド" % (ITEM_INFO[item]["price"]))
 
     def use_potion(self):
-        display_message("%sを使った。" % (self.ITEM_INFO[self.item_name]["name"]))
+        display_message("%sを使った。" % (ITEM_INFO[self.item_name]["name"]))
         self.change_stats()
         self.player.turn_item_used = True
 
     def change_stats(self):
-        for stat, stat_change in self.ITEM_INFO[self.item_name]["stat_change"]:
+        for stat, stat_change in ITEM_INFO[self.item_name]["stat_change"]:
             if (stat == "hp"):
                 self.heal_hp()
             # Continue from here.
 
     def heal_hp(self):
-        if (self.player.stats["hp"] + self.ITEM_INFO[self.item_name]["stat_change"]["hp"] > self.player.stats["max_hp"]):
+        if (self.player.stats["hp"] + ITEM_INFO[self.item_name]["stat_change"]["hp"] > self.player.stats["max_hp"]):
             heal = self.player.stats["max_hp"] - self.player.stats["hp"]
         self.player.stats["hp"] += heal
 
         display_message("体力が%s回復しました。" % (heal))
 
     def equip_item_on_player(self):
-        self.player.equipped_items[self.ITEM_INFO[self.item_name]["equip_region"]] = self.item_name
+        if ITEM_INFO[self.item_name]["equip_region"] == "both_hands":
+            self.player.equipped_items["left_hand"] = self.item_name
+            self.player.equipped_items["right_hand"] = self.item_name
+        else:
+            self.player.equipped_items[ITEM_INFO[self.item_name]["equip_region"]] = self.item_name
+            
+    def unequip_item_from_player(self):
+        if ITEM_INFO[self.item_name]["equip_region"] == "both_hands":
+            self.player.equipped_items["left_hand"] = "none"
+            self.player.equipped_items["right_hand"] = "none"
+        else:
+            self.player.equipped_items[ITEM_INFO[self.item_name]["equip_region"]] = "none"
 
     def add_equipment_stats(self):
-        for stat, stat_change in self.ITEM_INFO[self.item_name]["stat_change"].items():
+        for stat, stat_change in ITEM_INFO[self.item_name]["stat_change"].items():
             self.player.stats[stat] += stat_change
+            
+    def remove_equipment_stats(self):
+        for stat, stat_change in ITEM_INFO[self.item_name]["stat_change"].items():
+            self.player.stats[stat] -= stat_change
+            
+    def unequip_overlaping_equipment(self):
+        if ITEM_INFO[self.item_name]["region"] == "both_hands":
+            if (self.player.equipped_items["left_hand"] != "none") and (self.player.equipped_items["right_hand"] != "none"):
+                for region in ("left_hand", "right_hand"):
+                    if self.player.equipped_items[region] != "none":
+                        self.unequip_item(self.player.equipped_items[region])
+        elif self.player.equipped_items[ITEM_INFO[self.item_name]["region"]] != "none":
+            self.unequip_item(self.player.equipped_items[ITEM_INFO[self.item_name]["region"]])
 
     def equip_item(self):
         self.equip_item_on_player()
-
         self.add_equipment_stats()
 
     def unequip_item(self):
-        pass
+        self.remove_equipment_stats()
+        self.unequip_item_from_player()
+        display_message("%sを外しました。" % (ITEM_INFO[self.item_name]["name"]))
