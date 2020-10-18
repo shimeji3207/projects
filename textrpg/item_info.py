10a815c6ab783262caca720338b3857a37aa03ee
shimeji3207 <72081640+shimeji3207@users.noreply.github.com>
2020-10-18 16:21:47 +0900

    A new file to store item information.

---
+++ textrpg/item_info.py
@@ -0,0 +1,53 @@
+ITEM_INFO = {
+    "small_potion": {
+        "name": "小回復薬",
+        "price": 20,
+        "effect": "体力を20回復",
+        "type": "potion",
+        "stat_change": {
+            "hp": 20
+            }
+        },
+    "leather_chestplate": {
+        "name": "革のチェストプレート",
+        "price": 20,
+        "effect": "DEF+1",
+        "type": "equipment",
+        "equip_region": "chest",
+        "stat_change": {
+            "defence": 1
+            }
+        },
+    "sword": {
+        "name": "普通の剣",
+        "price": 20,
+        "effect": "ATK+1",
+        "type": "equipment",
+        "equip_region": "right_hand",
+        "stat_change": {
+            "attack": 1
+            }
+        },
+    "shield": {
+        "name": "普通の盾",
+        "price": 20,
+        "effect": "DEF+1, SPD-1",
+        "type": "equipment",
+        "equip_region": "left_hand",
+        "stat_change": {
+            "defence": 1
+            }
+        },
+    "claymore": {
+        "name": "普通のクレイモア",
+        "price": 20,
+        "effect": "ATK+3, SPD-1, DEF-1",
+        "type": "equipment",
+        "equip_region": "both_hands",
+        "stat_change": {
+            "attack": 3,
+            "defence": -1,
+            "speed": -1
+            }
+        }
+}
