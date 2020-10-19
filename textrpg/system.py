289b2c33d9cf671b75b9b8f547f1068166e839b7
shimeji3207 <72081640+shimeji3207@users.noreply.github.com>
2020-10-19 19:30:00 +0900

    Moved a function from town.py to here.

--- textrpg/system.py
+++ textrpg/system.py
@@ -1,2 +1,11 @@
 def display_message(text):
     input(text)
+
+def get_yes_no_input():
+    while (True):
+        command = input().strip().lower()
+
+        if command in ("y", "n"):
+            return command
+        else:
+            print("Invalid input")
